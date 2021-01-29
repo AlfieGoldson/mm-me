b7f73b57b2816c5bb4fb63f1ad69dc476dcfe747
Alfie <djobbo.maiga@gmail.com>
2021-01-29 14:03:56 +0100

    compressed img + favicon

--- pages/rn.js
+++ pages/rn.js
@@ -1,24 +1,32 @@
 import { useState } from 'react';
+import Head from 'next/head';
 
 export default function RN() {
 	const [open, setOpen] = useState(false);
 
-	return open ? (
+	return (
 		<>
-			<img
-				class='main-img'
-				src='/images/holy_words.png'
-				width='100%'
-				height='100%'
-			/>
-			<img
-				class='bg-img'
-				src='/images/holy_words.png'
-				width='100%'
-				height='100%'
-			/>
+			<Head>
+				<link rel='shortcut icon' href='/images/favicon.png' />
+			</Head>
+			{open ? (
+				<>
+					<img
+						class='main-img'
+						src='/images/holy_words.webp'
+						width='100%'
+						height='100%'
+					/>
+					<img
+						class='bg-img'
+						src='/images/holy_words.webp'
+						width='100%'
+						height='100%'
+					/>
+				</>
+			) : (
+				<h1 onClick={() => setOpen(true)}>Black</h1>
+			)}
 		</>
-	) : (
-		<h1 onClick={() => setOpen(true)}>Black</h1>
 	);
 }
