154c103fbf936d9514d4346c842431d609cccd17
Alfie <djobbo.maiga@gmail.com>
2021-01-29 13:54:39 +0100

    holy words

--- pages/rn.js
+++ pages/rn.js
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+
 export default function RN() {
-	return <h1>Black</h1>;
+	const [open, setOpen] = useState(false);
+
+	return open ? (
+		<>
+			<img
+				class='main-img'
+				src='/images/holy_words.png'
+				width='100%'
+				height='100%'
+			/>
+			<img
+				class='bg-img'
+				src='/images/holy_words.png'
+				width='100%'
+				height='100%'
+			/>
+		</>
+	) : (
+		<h1 onClick={() => setOpen(true)}>Black</h1>
+	);
 }
